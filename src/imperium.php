094360a6f6b0e73265e1c325e8a9a5e54e40d211
Yami Odymel <yamiodymel@gmail.com>
2015-12-09 18:56:40 +0000

    File with structure only.

---
+++ src/imperium.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+* Imperium Class
+*
+* @category  Tools
+* @package   Imperium
+* @author    Yami Odymel <yamiodymel@gmail.com>
+* @copyright Copyright (c) 2015
+* @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+* @link      http://github.com/TeaMeow/Imperium
+* @version   1.0
+**/
+
+session_start();
+
+class Imperium
+{
+    function addOrg()
+    {
+        
+    }
+    
+    function addRole()
+    {
+        
+    }
+    
+    
+    
+    
+    /***********************************************
+    /***********************************************
+    /************* R E S O U R C E S ***************
+    /***********************************************
+    /***********************************************
+     */
+     
+    function resOrg()
+    {
+        
+    }
+    
+    function resRole()
+    {
+        
+    }
+    
+    function resType()
+    {}
+    
+    function resId()
+    {
+        
+    }
+    
+    
+    
+    
+    /***********************************************
+    /***********************************************
+    /************ A L L O W & D E N Y **************
+    /***********************************************
+    /***********************************************
+    
+    /**
+     * Allow
+     */
+    
+    function allow()
+    {
+        
+    }
+    
+    function deny()
+    {
+        
+    }
+    
+    function allowed()
+    {
+        
+    }
+    
+    function denied()
+    {
+        
+    }
+
+
+
+
+    /***********************************************
+    /***********************************************
+    /************ C A N & C A N N O T **************
+    /***********************************************
+    /***********************************************
+
+    /*
+     */
+    
+    function can()
+    {
+        
+    }
+    
+    function cannot()
+    {
+        
+    }
+}
+?>
