eb20a2fa861b7204bc9311d3d5d4d0736012ce61
Yami Odymel <yamiodymel@gmail.com>
2015-12-10 18:50:58 +0000

    Testing with addX, resX, pointers, and assign functions.

--- test/index.php
+++ test/index.php
@@ -7,7 +7,10 @@ $imperium = new Imperium();
 $imperium->addOrg('網站')
          ->addRole('管理員')
          ->addRole('版主')
-         ->addRole('使用者');
+         ->addRole('使用者')
+         ->caller(1)
+         ->org('網站')
+         ->assign(['管理員', '版主']);
          
 $imperium->org('網站')
          ->role('管理員')
