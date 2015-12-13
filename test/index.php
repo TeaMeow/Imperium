ca939b763c7d501aac40f100b69b303a88073b67
Yami Odymel <yamiodymel@gmail.com>
2015-12-13 16:58:47 +0000

    Sorted the folder.

--- test/index.php
+++ test/index.php
@@ -8,16 +8,12 @@ $imperium->addOrg('網站')
          ->addRole('管理員')
          ->addRole('版主')
          ->addRole('使用者')
-         ->caller(1)
-         ->org('網站')
-         ->assign(['管理員', '版主']);
+         ->caller(1);
          
 $imperium->self()
-         ->resType('相簿')
-         ->resId(3)
-         ->allow('%');
+         ->allow('編輯', '相簿', 13);
          
   
 
-exit(var_dump($imperium->resType('相簿')->resId(3)->cannot('d')));
+exit(var_dump($imperium->can('編輯', '相簿')));
 ?>
