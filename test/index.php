a5fb0188677d3ee8203d7a895b1d0f4feeb2cfc9
Yami Odymel <yamiodymel@gmail.com>
2015-12-14 07:05:04 +0000

    Test with the aliases.

--- test/index.php
+++ test/index.php
@@ -9,11 +9,20 @@ $imperium->addOrg('網站')
          ->addRole('版主')
          ->addRole('使用者')
          ->caller(1);
-         
-$imperium->self()
-         ->allow('編輯', '相簿', 13);
-         
-  
 
-exit(var_dump($imperium->can('編輯', '相簿')));
+$imperium->org('網站')
+         ->assign('管理員');
+         
+$imperium->org('網站')
+         ->role('使用者')
+         ->alias('管理', ['編輯', '新增', '移除'])
+         ->deny('管理', '%');         
+         
+$imperium->org('網站')
+         ->alias('管理', ['編輯', '新增', '移除'])
+         ->allow('管理', '%');
+         
+
+
+exit(var_dump($imperium->can('新增', '相簿')));
 ?>
